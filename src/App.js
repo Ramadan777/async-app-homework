f4110d6220d9a35b6f6a977f91168bd8b5f88687
Ramadan777 <faysol@mail.ru>
2020-11-19 16:18:47 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => {
+          setTodos(json);
+      })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {todos.map(todos => {
+        return (
+          <div className={`content ${todos.completed ? 'selected' : ''}`}>
+            <table border='1'>
+              {todos.title}
+            </table>
+          </div>
+        )
+      })}
     </div>
   );
 }
